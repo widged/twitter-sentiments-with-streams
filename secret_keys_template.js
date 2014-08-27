d5e3d32183d06125e9db20900d9843baa8a71312
Marielle Lange <mlange@widged.com>
2014-08-27 22:37:30 +1200

    secret_keys template

---
+++ secret_keys_template.js
@@ -0,0 +1,9 @@
+// Add your keys and rename this file to `secret_keys.js`
+module.exports = {
+	twitter : {
+        consumer_key: "YOUR_CONSUMER_KEY",
+        consumer_secret: "YOUR_CONSUMER_SECRET",
+        access_token_key: "USER_ACCESS_TOKEN",
+        access_token_secret: "USER_ACCESS_TOKEN_SECRET"
+	}
+};
